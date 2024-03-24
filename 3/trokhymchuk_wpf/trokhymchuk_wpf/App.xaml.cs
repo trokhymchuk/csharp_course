369d74f2dd0a937da21d87e68c19c4ca0acd22dd
Artem Trokhymchuk <66204814+trokhymchuk@users.noreply.github.com>
2024-03-24 12:53:35 -0700

    add 3

---
+++ 3/trokhymchuk_wpf/trokhymchuk_wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KMA_ProgrammingCsharp2024
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
